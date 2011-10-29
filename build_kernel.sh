3d137b53485fed149d7ef96d538216edf8570bfd
jt1134 <jt1134@gmail.com>
2011-10-29 15:57:37 -0500

    switch to 2 stage init
    Change-Id: I338da117e42819704b85a42600260d4b56d706e7

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,24 +11,11 @@ make ARCH=arm p1_defconfig
 make -j8 modules
 for M in `find . -name *.ko`
 do
-  B=`basename "$M"`
-  case "$B" in
-  "dhd.ko")
-    cp "$M" ../../../"$TREE"
-    ;;
-  *)
-    cp "$M" ../../../"$TREE"/initramfs/lib/modules
-    ;;
-  esac
+  cp "$M" ../../../"$TREE"/modules
 done
 
 make -j8
 cp arch/arm/boot/zImage ../../../"$TREE"/kernel
 
 mv DONTLOOKATME .git
-
-X="arch/arm/boot/zImage"
-adb reboot download
-sleep 5
-heimdall flash --kernel "$X" --recovery "$X"
 
